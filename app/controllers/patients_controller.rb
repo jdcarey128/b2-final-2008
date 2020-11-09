33860d22059322832ddfc28f2f3e596aafb5de44
Joshua Carey <jdcarey128@gmail.com>
2020-11-09 10:55:44 -0700

    Add patient controller destroy action

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,11 @@
+class PatientsController < ApplicationController
+
+  def destroy
+    hospital = Hospital.find(params[:hospital_id])
+    doctor = Doctor.find(params[:id])
+    patient_doctor = PatientDoctor.find_by(doctor_id: doctor, patient_id: params[:patient_id])
+    patient_doctor.destroy
+    redirect_to doctor_path(hospital, doctor)
+  end
+
+end
