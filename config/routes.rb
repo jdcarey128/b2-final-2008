eeb31189b8fc59704ce5c54d348ac29addcfd77a
Joshua Carey <jdcarey128@gmail.com>
2020-11-09 10:21:25 -0700

    Add hospital show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :hospitals, only: [:show]
+
   scope '/hospitals/:id' do
     resources :doctors, only: [:show]
   end
